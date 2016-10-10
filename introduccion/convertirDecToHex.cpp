e35088392630c6c1f7c53ebaa3a595f1e50be787
Moises <moiseserg@gmail.com>
2016-10-10 13:22:38 -0500

    Update convertirDecToHex.cpp
    Usando operadores de corrimiento y máscaras
    Sin ciclos
    
    Dígito por dígito en hexadecimal (bloques de 4 bits en binario = bloques de 1 dígito en hex)

--- introduccion/convertirDecToHex.cpp
+++ introduccion/convertirDecToHex.cpp
@@ -1,3 +1,12 @@
+
+/*
+Convertir un valor de base 10 a hex
+
+Usando operadores de corrimiento y máscaras
+Sin ciclos
+Dígito por dígito en hexadecimal (bloques de 4 bits en binario = bloques de 1 dígito en hex)
+*/
+
 #include <stdio.h>
 
 int main(){
