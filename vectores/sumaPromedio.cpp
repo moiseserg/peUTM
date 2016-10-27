b7e5a4a2f34e644e142690c4e0adfe898d2f6865
Moises <moiseserg@gmail.com>
2016-10-27 11:19:07 -0500

    lee y muestra un vector/ calcula suma y promedio

--- vectores/sumaPromedio.cpp
+++ vectores/sumaPromedio.cpp
@@ -2,14 +2,25 @@
 int main (){
 	int a, i=0, v[100],s;
 	double pro;
-		while (i<100){
-			scanf("%d ", &v[i]);
+	//ciclo para pedir valores
+	while (i<100){
+		//lectura
+		scanf("%d ", &v[i]);
+		i++;
+	}
+	
+	i=0;
+	while (i<100){
+		//muestra cada valor
 		printf("v[%d] = %d\n", i, v[i]);
-			s= s + v[i];
-			i++;
-		}
-		printf("Sumatoria: %d\n", s);
-		pro = s/100;
-		printf("Promedio: %.2f", (double)pro);
+		//acumula en s
+		s= s + v[i];
+		i++;
+	}
+
+	
+	printf("Sumatoria: %d\n", s);
+	pro = s/100.0;
+	printf("Promedio: %.2f", (double)pro);
 	return 0;
 }
