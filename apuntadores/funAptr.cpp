7921a8703b464f2764fb4febe5cf64fcfe379d6a
Moisés Ramírez <moiseserg@github.com>
2016-11-08 08:56:56 -0600

    ejemplo funciones con apuntadores

--- apuntadores/funAptr.cpp
+++ apuntadores/funAptr.cpp
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
 int incrementa1(int x){
-	printf("Antes de cambiar (fun) %p %d\n",&x, x );
+	printf("Antes de cambiar (fun)x %p %d\n",&x, x );
 	x++;
-	printf("Despues de cambiar (fun) %p %d\n", &x,  x );
+	printf("Despues de cambiar (fun)x %p %d\n", &x,  x );
 }
 
 int incrementa2(int *x){
-	printf("Antes de cambiar (fun2) %p %d\n",x, *x );
+	printf("Antes de cambiar (fun2)x %p %d\n",x, *x );
 	(*x)++;
-	printf("Despues de cambiar (fun2) %p %d\n", x,  *x );
+	printf("Despues de cambiar (fun2)x %p %d\n", x,  *x );
 }
 
 int main(){
 	
 	int a = 10;
-	printf("En main antes de incrementa1 %p %d\n", &a, a );
+	printf("En main antes de incrementa1 (a) %p %d\n", &a, a );
 	incrementa1(a);
-	printf("En main despues de incrementa1 %p %d\n", &a, a );
+	printf("En main despues de incrementa1  (a)  %p %d\n", &a, a );
 
-	printf("\nEn main antes de incrementa2 %p %d\n", &a, a );
+	printf("\nEn main antes de incrementa2  (a) %p %d\n", &a, a );
 	incrementa2(&a);
-	printf("En main despues de incrementa2 %p %d\n", &a, a );
+	printf("En main despues de incrementa2  (a) %p %d\n", &a, a );
 
 
 }
