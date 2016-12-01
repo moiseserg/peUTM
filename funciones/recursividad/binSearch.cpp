16f80757f9d0a5802ef0a8f253f438e8acfbbd84
Moisés Ramírez <moiseserg@github.com>
2016-12-01 12:01:17 -0600

    update

--- funciones/recursividad/binSearch.cpp
+++ funciones/recursividad/binSearch.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int bbinaria(int v[], int izq, int der, int busca){
+
 
 }
 
 
-
-
-
+int mainx(){
+	int v[10] = {2,3,4,5,9,12,15,23,28,30};
+	for(int i=0; i<=32; i++){
+		printf("%d\n",  bbinaria(v, 0, 9, i));			
+	}
+}
 
 
 int compara(const void *p, const void *q){
@@ -16,16 +21,24 @@ int compara(const void *p, const void *q){
 	return *pp - *qq;
 }
 
-
-
-
 int main(){
 	int v[10] = {2,3,4,5,9,12,15,23,28,30};
 	for(int i=0; i<=32; i++){
+		int *ptr =(int *)bsearch(&i, v, 10, sizeof(int), compara);
 
+		if(ptr)
+			printf("%d está en la pos %d\n", i,  (int)(ptr-v) );		
+		else
+			printf("%d no está en el vector\n", i );
 		
 	}
 
-		//printf("%d\n",  bbinaria(v, 0, 9, i));		
+	//printf("%d\n",  bbinaria(v, 0, 9, i));		
 }
+
+
+
+
+
+
 
