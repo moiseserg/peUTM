77f42342a39e3ddcc0cfc723d22afb8669a57c28
Moisés Ramírez <moiseserg@github.com>
2016-12-14 08:30:02 -0600

    dkjkjadskjdsa

--- estructuras/ejemplo3.cpp
+++ estructuras/ejemplo3.cpp
@@ -11,7 +11,7 @@ typedef struct persona{
 int main(){
 	P p;
 	P q[10];
-	P r={"juan", 20, 79, 1.65};		
+	P r={"pedro", 20, 79, 1.65};		
 	P s[10]= { {"juan", 20, 79, 1.65}, 
 							{"juan", 20, 79, 1.65}
 						};
@@ -20,4 +20,25 @@ int main(){
 							{"juan", 20, 79, 1.65}
 						};
 
+	printf("%d\n", (int)sizeof(P) );
+
+	scanf("%s", p.nombre);
+	scanf("%d", &p.edad);
+
+	printf("%s %d %f %f\n", p.nombre, p.edad, 
+		p.peso, p.estatura );
+
+	p = r;
+
+	printf("%s %d %f %f\n", p.nombre, p.edad, 
+		p.peso, p.estatura );
+
+	p = s[0];
+
+	printf("%s %d %f %f\n", p.nombre, p.edad, 
+		p.peso, p.estatura );
+
+
 }
+
+
